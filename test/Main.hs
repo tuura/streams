bc66941f9d12ce5678925574530f96175110dee0
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-02-17 14:14:09 +0000

    Add streams with functions

--- test/Main.hs
+++ test/Main.hs
@@ -1,14 +1,19 @@
 import Tuura.Stream
 
+-- Various event sources
 chars :: [Event Char]
-chars = map Event "abcdefghij"
+chars = map Event "abcde"
 
 posInts :: [Event Int]
-posInts = map Event [1..10]
+posInts = map Event [1..5]
 
 negInts :: [Event Int]
 negInts = map (fmap negate) posInts
 
+bools :: [Event Bool]
+bools = map Event $ cycle [True, False]
+
+-- Test streams
 test :: Stream (Char, String)
 test = Join 1 (Source 2 chars) (Map 3 show $ Source 4 posInts)
 
@@ -20,8 +25,19 @@ testGet = case getStream 3 test of
 testSet :: Stream (Char, String)
 testSet = setStream 2 (Source 5 negInts) test
 
+testFun :: Stream (Bool -> String)
+testFun = Map 6 choose test
+  where
+    choose (c, s) = \x -> if x then c:[] else s
+
+testApp :: Stream String
+testApp = Map 7 app $ Join 8 testFun (Source 9 bools)
+  where
+    app (f, x) = f x
+
 main :: IO ()
 main = do
     putStrLn $ "test    = " ++ show (eval test   )
     putStrLn $ "testGet = " ++ show (eval testGet)
     putStrLn $ "testSet = " ++ show (eval testSet)
+    putStrLn $ "testApp = " ++ show (eval testApp)
